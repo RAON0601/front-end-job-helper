18d38b1350378eac452901f2661f39592690fa08
김정민 <68500898+RAON0601@users.noreply.github.com>
2023-01-23 16:49:39 +0900

    채용공고 페이지 로딩 및 에러 상태 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,44 @@
 import * as React from "react";
-import type { GetServerDataReturn, HeadFC, PageProps } from "gatsby";
+import type { HeadFC } from "gatsby";
 import { Link } from "gatsby";
 import { Container } from "../components/Container";
 import { Table } from "../components/Table";
 import { Title } from "../components/Title";
 
-const IndexPage = ({ serverData }: PageProps) => {
-  const infos = (serverData as ServerDataProps).infos;
+type InfoType = {
+  company: string;
+  position: string;
+  link: string;
+};
+
+const IndexPage = () => {
+  const [infos, setInfos] = React.useState<InfoType[]>([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
+
   const date = new Date();
   const year = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
+
+  React.useEffect(() => {
+    const fetchInfoData = async () => {
+      try {
+        const res = await fetch(
+          "https://jungmini.s3.ap-northeast-2.amazonaws.com/info.json"
+        );
+        setLoading(true);
+        const data = (await res.json()) as InfoType[];
+        setInfos(data);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        setError(true);
+      }
+    };
+
+    fetchInfoData();
+  }, []);
 
   return (
     <Container>
@@ -20,6 +48,8 @@ const IndexPage = ({ serverData }: PageProps) => {
 
       <Link to="/interview">인터뷰 연습하기</Link>
 
+      {loading && <h1>로딩중..</h1>}
+      {error && <h1>에러!</h1>}
       <Table>
         <thead>
           <tr>
@@ -29,15 +59,14 @@ const IndexPage = ({ serverData }: PageProps) => {
           </tr>
         </thead>
         <tbody>
-          {infos.map(({ company, position, link }) => (
-            <tr>
+          {infos.map(({ company, position, link }, index) => (
+            <tr key={index}>
               <td>{company}</td>
               <td>{position}</td>
               <td>
-                {" "}
-                <Link to={link} target="_blank">
+                <a href={link} target="_blank">
                   {link}
-                </Link>
+                </a>
               </td>
             </tr>
           ))}
@@ -50,26 +79,3 @@ const IndexPage = ({ serverData }: PageProps) => {
 export default IndexPage;
 
 export const Head: HeadFC = () => <title>채용공고 모아보기</title>;
-
-type InfoType = {
-  company: string;
-  position: string;
-  link: string;
-};
-
-type ServerDataProps = {
-  infos: InfoType[];
-};
-
-export async function getServerData(): GetServerDataReturn<ServerDataProps> {
-  const res = await fetch(
-    "https://jungmini.s3.ap-northeast-2.amazonaws.com/info.json"
-  );
-  const data = (await res.json()) as InfoType[];
-
-  return {
-    props: {
-      infos: data,
-    },
-  };
-}
